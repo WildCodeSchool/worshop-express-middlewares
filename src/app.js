69ec9fd70ae4cc0a6ffa22cfff22ef374e3c1b13
mfalguierwcs <maxime.falguier@wildcodeschool.com>
2023-01-23 22:03:39 +0100

    workshop done

--- src/app.js
+++ src/app.js
@@ -2,14 +2,62 @@ const express = require('express');
 
 const app = express();
 
-const items = [
-  { id: 1, name: 'item1' },
-  { id: 2, name: 'item2' },
-];
+const middleware1 = (req, res, next) => {
+  console.log('doing stuff in middleware 1');
+  next();
+};
 
-app.get('/things', (req, res) => {
-  console.log('handling GET /things');
-  res.send(items);
+const middleware2 = (req, res, next) => {
+  console.log('doing stuff in middleware 2');
+  next();
+};
+
+app.get('/myroute', middleware1, middleware2, (req, res) => {
+  console.log('handling /myroute');
+  res.send('content for /myroute');
 });
+
+const extractName = (req, resp, next) => {
+  if (req.query.name) {
+    req.reqName = req.query.name;
+    next();
+  } else {
+    resp.status(400).send('Oups le param name est obligatoire !');
+  }
+};
+
+app.get('/hello', extractName, (req, res) => {
+  console.log('handling /hello');
+  res.send(`Hello ${req.reqName} !`);
+});
+
+app.get('/syncError', (req, res) => {
+  throw new Error('BOOM');
+});
+
+app.get('/asyncError', (req, res, next) => {
+  setTimeout(() => {
+    // essaye de réaliser quelques chose js
+    try {
+      // on provoque une erreur
+      throw new Error('BOOM', { cause: 'timeoutError' });
+    } catch (err) {
+      // on attrape l'erreur
+      console.log(err);
+      next(err); // on passe cette erreur au middleware de gestion d'erreur
+    }
+  }, 1000);
+});
+
+const handleError = (err, req, res, next) => {
+  // selon la nature de l'erreur on envoye des réponses lié au contexte de l'erreur
+  if (err.cause === 'timeoutError') {
+    res.status(501).send('Erreur inconue de type timeout');
+  }
+  // si pas de réponse explicite, envoie d'une erreur par défaut (500)
+  res.status(500).send('internal serveur erreur');
+};
+
+app.use(handleError);
 
 module.exports.app = app;
